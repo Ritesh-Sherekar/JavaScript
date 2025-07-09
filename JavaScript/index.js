f693f22639f00461c2849a4bf2b4ce0f5902e046
Ritesh <riteshsherekar69@gmail.com>
2025-07-09 18:54:29 +0530

    Add checked and inc and desc code

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -14,22 +14,65 @@
 //     document.getElementById("myH").textContent = `welcome, ${username}`;
 // }
 
-const desc = document.getElementById("dec");
-const inc = document.getElementById("inc");
-const reset = document.getElementById("reset");
-const webCount = document.getElementById("myLable");
 
-let count =0;
+// for inc and desc task
 
-inc.onclick = function(){
-    count++;
-    webCount.textContent = count;
-}
-desc.onclick = function(){
-    count--;
-    webCount.textContent = count;
-}
-reset.onclick = function(){
-    count = 0;
-    webCount.textContent = count;
+// const desc = document.getElementById("dec");
+// const inc = document.getElementById("inc");
+// const reset = document.getElementById("reset");
+// const webCount = document.getElementById("myLable");
+
+// let count =0;
+
+// inc.onclick = function(){
+//     count++;
+//     webCount.textContent = count;
+// }
+// desc.onclick = function(){
+//     count--;
+//     webCount.textContent = count;
+// }
+// reset.onclick = function(){
+//     count = 0;
+//     webCount.textContent = count;
+// }
+
+
+// Random number generator
+
+// const lable = document.getElementById("myID");
+// const myBTN = document.getElementById("myBTN");
+// let random;
+
+// myBTN.onclick = function(){
+//     random = Math.floor(Math.random() * 6) + 1;
+//     lable.textContent = random;
+// }
+
+// for checked properties
+
+const myCh = document.getElementById("myCh");
+const visa = document.getElementById("visa");
+const master = document.getElementById("master");
+const Pay = document.getElementById("Pay");
+const submit = document.getElementById("submit");
+const forS = document.getElementById("forS");
+const forP = document.getElementById("forP");
+
+submit.onclick = function(){
+    if(myCh.checked){
+        forS.textContent = "You are Scbscribe";
+    }else{
+         forS.textContent = "You are not Scbscribe";
+    } 
+    
+    if(visa.checked){
+        forP.textContent = "Pay using visa";
+    }else if(master.checked){
+        forP.textContent = "Pay using MasterCart";
+    }else if(Pay.checked){
+        forP.textContent = "Pay using PayPal";
+    }else{
+         forP.textContent = "Select one of the payment mode";
+    }
 }
