9dfc8ab85545dffe3010a26fa6f76584e1fc9940
Ritesh <riteshsherekar69@gmail.com>
2025-07-10 18:50:56 +0530

    Add code regarding String Method

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -51,28 +51,49 @@
 
 // for checked properties
 
-const myCh = document.getElementById("myCh");
-const visa = document.getElementById("visa");
-const master = document.getElementById("master");
-const Pay = document.getElementById("Pay");
-const submit = document.getElementById("submit");
-const forS = document.getElementById("forS");
-const forP = document.getElementById("forP");
+// const myCh = document.getElementById("myCh");
+// const visa = document.getElementById("visa");
+// const master = document.getElementById("master");
+// const Pay = document.getElementById("Pay");
+// const submit = document.getElementById("submit");
+// const forS = document.getElementById("forS");
+// const forP = document.getElementById("forP");
 
-submit.onclick = function(){
-    if(myCh.checked){
-        forS.textContent = "You are Scbscribe";
-    }else{
-         forS.textContent = "You are not Scbscribe";
-    } 
+// submit.onclick = function(){
+//     if(myCh.checked){
+//         forS.textContent = "You are Scbscribe";
+//     }else{
+//          forS.textContent = "You are not Scbscribe";
+//     } 
     
-    if(visa.checked){
-        forP.textContent = "Pay using visa";
-    }else if(master.checked){
-        forP.textContent = "Pay using MasterCart";
-    }else if(Pay.checked){
-        forP.textContent = "Pay using PayPal";
-    }else{
-         forP.textContent = "Select one of the payment mode";
-    }
-}
+//     if(visa.checked){
+//         forP.textContent = "Pay using visa";
+//     }else if(master.checked){
+//         forP.textContent = "Pay using MasterCart";
+//     }else if(Pay.checked){
+//         forP.textContent = "Pay using PayPal";
+//     }else{
+//          forP.textContent = "Select one of the payment mode";
+//     }
+// }
+
+// for method chaining
+
+let username = window.prompt("Enter your username ");
+
+// without method chainning
+// username = username.trim();
+// let letter = username.charAt(0);
+// letter = letter.toUpperCase();
+
+// let extra = username.slice(1);
+// extra = extra.toLowerCase();
+
+// username = letter + extra;
+
+// console.log(username);
+
+// with method chainning
+
+username = username.trim().charAt(0).toUpperCase() + username.trim().slice(1).toLowerCase();
+console.log(username);
